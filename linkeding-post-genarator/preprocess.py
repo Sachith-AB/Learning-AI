4c367c37f7db9bfc53b8fa96def0c9496763f7e5
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-06-08 18:07:07 +0530

    create language recommendation

--- linkeding-post-genarator/preprocess.py
+++ linkeding-post-genarator/preprocess.py
@@ -1,4 +1,8 @@
 import json
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from llm_helper import llm
 
 def process_posts(raw_file_path, process_file_path="data/processed_posts.json"):
     details_posts = []
@@ -8,17 +12,65 @@ def process_posts(raw_file_path, process_file_path="data/processed_posts.json"):
         metadata = extract_metadata(post['text'])
         post_with_metadata = post | metadata
         details_posts.append(post_with_metadata)
-        
-        print(details_posts[0])
+    
+    langs = get_all_languages(details_posts)
+    for lang in langs:
+        print(lang)
 
+
+def get_all_languages(posts):
+    languages = set()
+    for post in posts:
+        if 'tech_stack' in post:
+            for category in ['front_end', 'back_end', 'additional']:
+                if category in post['tech_stack']:
+                    languages.update(post['tech_stack'][category])
+    
+    template = '''You are a technology recommender. For the given technologies:
+    {languages}
+    
+    Return a JSON object with:
+    - "frontend_recommendations": Array of suggested frontend technologies
+    - "backend_recommendations": Array of suggested backend technologies
+    - "database_recommendations": Array of suggested databases
+    - "architecture_recommendations": Array of suggested architectures
+    
+    Return ONLY valid JSON, no additional text. Example:
+    {{
+        "frontend_recommendations": [...],
+        "backend_recommendations": [...]
+    }}'''
+    
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm | JsonOutputParser()
+    
+    try:
+        return chain.invoke({'languages': list(languages)})
+    except Exception as e:
+        print(f"Error generating recommendations: {str(e)}")
+        return {}
 
 def extract_metadata(post):
-    return {
-        'line_count': 10,
-        'problem': 'problem',
-        'solution': 'solution'
-        
-    }
+    template = '''
+        You are given linkedin post about software projects. You need to extract number of lines,
+        problem that solve from that software and what is the solution given from that software.
+        Need json object with 3 keys called line_count, problem and solution.
+        no preamble need
+        Here is the actual post on with you need to perform that task:
+        {post}
+    '''
+    
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={'post': post})
+    
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs")
+    
+    return res
 
 
 if __name__  == "__main__":
